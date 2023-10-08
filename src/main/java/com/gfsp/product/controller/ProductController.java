e31b041bd577f0aa04fd8780ab081d9a9fdbb2bd
Mohammad Darwish <mohammad.darwish@ericsson.com>
2023-10-08 13:56:20 +0200

    Add endpoints descriptions with response description

--- src/main/java/com/gfsp/product/controller/ProductController.java
+++ src/main/java/com/gfsp/product/controller/ProductController.java
@@ -2,6 +2,8 @@ package com.gfsp.product.controller;
 
 import com.gfsp.product.dto.ProductDTO;
 import com.gfsp.product.service.Impl.ProductServiceImp;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -16,7 +18,11 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "api/v1/products", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(
+    path = "api/v1/products",
+    produces = MediaType.APPLICATION_JSON_VALUE,
+    consumes = MediaType.APPLICATION_JSON_VALUE
+)
 @AllArgsConstructor
 @RequiredArgsConstructor
 @Validated
@@ -25,26 +31,58 @@ public class ProductController {
     @Autowired
     private ProductServiceImp productService;
 
+    @Operation(
+        summary = "ADD products rest API",
+        description = "ADD products endpoint to add products to the database")
+    @ApiResponse(
+        responseCode = "201",
+        description = "HTTP status is 201 created")
     @PostMapping
-    public ResponseEntity<List<ProductDTO>> addProducts(@Valid @RequestBody List<ProductDTO> productDTOS) {
+    public ResponseEntity<List<ProductDTO>> addProducts(@Valid @RequestBody
+                                                        List<ProductDTO> productDTOS) {
         List<ProductDTO> createdProducts = productService.addProducts(productDTOS);
         return new ResponseEntity<>(createdProducts, HttpStatus.CREATED);
     }
 
+    @Operation(
+        summary = "GET product by id restAPI",
+        description = "GET product by id to get product id that matches the path variable")
+    @ApiResponse(
+        responseCode = "200",
+        description = "HTTP status code is 200 okay")
     @GetMapping("{id}")
-    public ResponseEntity<ProductDTO> getProductById(@Valid @PathVariable String id) {
+    public ResponseEntity<ProductDTO> getProductById(@Valid @PathVariable
+                                                     String id) {
         ProductDTO productByID = productService.getProductByID(id);
         return new ResponseEntity<>(productByID, HttpStatus.OK);
     }
 
+    @Operation(
+        summary = "GET products restAPI",
+        description = "GET products to get all products with possibility to add filters using parameters"
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "HTTP status code is 200 okay"
+    )
     @GetMapping
-    public ResponseEntity<List<ProductDTO>> getProducts(@Valid @RequestParam MultiValueMap<String, String> productFilters) {
+    public ResponseEntity<List<ProductDTO>> getProducts(@Valid @RequestParam
+                                                        MultiValueMap<String, String> productFilters) {
         List<ProductDTO> products = productService.getProducts(productFilters);
         return new ResponseEntity<>(products, HttpStatus.OK);
     }
 
+    @Operation(
+        summary = "DELETE product by ID restAPI",
+        description = "DELETE product by ID is to remove a product from the database."
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "HTTP status code is 200 okay"
+    )
     @DeleteMapping("{id}")
-    public ResponseEntity<ProductDTO> deleteProduct(@Valid @PathVariable String id) {
+    public ResponseEntity<ProductDTO> deleteProduct(@Valid @PathVariable
+                                                    String id) {
         boolean isDeleted = productService.deleteProductById(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
